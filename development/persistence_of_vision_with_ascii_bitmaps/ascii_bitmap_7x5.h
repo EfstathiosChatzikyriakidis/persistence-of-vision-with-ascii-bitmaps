56339ff8c5ad257e308367fdcd9f98e989d772b8
Efstathios Chatzikyriakidis <stathis.chatzikyriakidis@gmail.com>
2022-04-25 00:38:45 +0300

    Change.

--- development/persistence_of_vision_with_ascii_bitmaps/ascii_bitmap_7x5.h
+++ development/persistence_of_vision_with_ascii_bitmaps/ascii_bitmap_7x5.h
@@ -1,7 +1,7 @@
 /*
  *  ASCII Character Set Of Printable Only Characters (As Bitmap Array).
  *
- *  Copyright (C) 2010 Efstathios Chatzikyriakidis (contact@efxa.org)
+ *  Copyright (C) 2010 Efstathios Chatzikyriakidis (stathis.chatzikyriakidis@gmail.com)
  *
  *  This program is free software: you can redistribute it and/or modify
  *  it under the terms of the GNU General Public License as published by
